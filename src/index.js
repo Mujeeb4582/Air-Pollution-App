244b2b032f5dd509698bd353691f441770589604
Mujeeb4582 <mujeeburahman4582@gmail.com>
2023-01-05 21:02:18 +0500

    Add the redux store

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
-import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
