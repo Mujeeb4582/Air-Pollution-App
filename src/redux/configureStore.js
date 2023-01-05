fb1d0be4758b808d8b48bde69606bded80c79799
Mujeeb4582 <mujeeburahman4582@gmail.com>
2023-01-05 21:06:07 +0500

    refactor the code

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-// import logger from 'redux-logger';
 import pollutionReducer from './pollution-reducer/pollutionSlice';
 
 const store = configureStore({
   reducer: {
     airPollution: pollutionReducer,
   },
-  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
