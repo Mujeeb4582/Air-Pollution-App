e92d04659183561b98695c298ca30ab2b9bd73db
Mujeeb4582 <mujeeburahman4582@gmail.com>
2023-01-05 21:03:34 +0500

    Add the Navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
+import { IoIosArrowBack } from 'react-icons/io';
+import { FiSettings } from 'react-icons/fi';
+import { BsMic } from 'react-icons/bs';
+
+function Navbar() {
+  const location = useLocation();
+  return (
+    <header className="d-flex justify-content-between p-3 fs-2 fw-bold">
+      <div>
+        <NavLink to="/">
+          <IoIosArrowBack
+            className="me-3 fs-1"
+            style={{ color: 'white' }}
+          />
+        </NavLink>
+      </div>
+      <div>
+        {location.pathname !== '/' ? 'Details Report' : 'Air Pollution App'}
+      </div>
+      <div>
+        <BsMic className="mx-3" />
+        <FiSettings />
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
